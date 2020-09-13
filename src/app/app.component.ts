6fa987f712ceb4e194008f947f990999f2c18695
FazioNico <contact@nicolasfazio.ch>
2020-09-13 11:09:05 +0200

    upd with front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import videojs from 'video.js';
+import awsvideoconfig from '../aws-video-exports';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'NgAmplified';
+  player: videojs.Player;
+  @ViewChild('videoNode') videoNode: ElementRef<HTMLVideoElement>;
+  options = {
+    autoplay: true,
+    controls: true,
+    sources: [
+      {
+        src: awsvideoconfig.awsOutputLiveLL,
+      },
+    ],
+  };
+
+  ngAfterViewInit(): void {
+    this.player = videojs(this.videoNode.nativeElement, this.options);
+  }
 }
